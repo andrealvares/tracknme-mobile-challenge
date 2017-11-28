565f939879298389399ba863e1e2434bf42ac803
Thales Frigo <thalesfrigo@gmail.com>
2017-11-28 07:40:54 -0200

    Install SwiftyJSON and Moya

---
+++ DevTrackNMe/DevTrackNMe/Extensions/Bundle+LoadResource.swift
@@ -0,0 +1,9 @@
+//
+//  Bundle+LoadResource.swift
+//  DevTrackNMe
+//
+//  Created by Thales Frigo on 26/11/17.
+//  Copyright © 2017 Thales Frigo. All rights reserved.
+//
+
+import Foundation
---
+++ DevTrackNMe/DevTrackNMe/Run/Coordinate.swift
@@ -0,0 +1,9 @@
+//
+//  Coordinate.swift
+//  DevTrackNMe
+//
+//  Created by Thales Frigo on 26/11/17.
+//  Copyright © 2017 Thales Frigo. All rights reserved.
+//
+
+import Foundation
